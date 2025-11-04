01e9d4ba373950117b9a826ff8f1e65b5c2c5cb5
Ana Ramirez <daniel805228@gmail.com>
2025-11-03 19:41:16 -0500

    feat: Add end-points for login and register users

--- BackendLuminaDev/src/routes/index.js
+++ BackendLuminaDev/src/routes/index.js
@@ -1,13 +1,13 @@
-﻿const express = require("express");
-const router = express.Router();
-const users = require("./users");
-const products = require("./productRoutes");
+﻿  const express = require("express");
+  const router = express.Router();
+  const users = require("./users");
+  const products = require("./productRoutes");
 
-router.use("/users", users);
-router.use("/products", products);
+  router.use("/users", users);
+  router.use("/products", products);
 
-router.get("/", (req, res) => {
-  res.json({ mensaje: "¡Bienvenido a mi API con Express!" });
-});
+  router.get("/", (req, res) => {
+    res.json({ mensaje: "¡Bienvenido a mi API con Express!" });
+  });
 
-module.exports = router;
+  module.exports = router;
